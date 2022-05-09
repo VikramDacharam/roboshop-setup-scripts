5e2008e0ae239a7cb6a6e003c9c3b56d71e70846
Vikram691 <dasaramvikram@gmail.com>
2022-05-09 16:18:33 +0530

    12

--- components/frontend.sh
+++ components/frontend.sh
@@ -13,29 +13,29 @@ checkRootUser
  statusCheck $?
 
  echo "Downloading frontend code"
- curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >>$(LOG_FILE)
+ curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >>$LOG_FILE
  statusCheck $?
 
  cd /usr/share/nginx/html
 
  echo "remove old files"
- rm -rf * >>$(LOG_FILE)
+ rm -rf * >>$LOG_FILE
  statusCheck $?
 
  echo "extracting zip content"
- unzip /tmp/frontend.zip >>$(LOG_FILE)
+ unzip /tmp/frontend.zip >>$LOG_FILE
  statusCheck $?
 
  echo "move files"
- mv frontend-main/* . >>$(LOG_FILE)
- mv static/* . >>$(LOG_FILE)
- rm -rf frontend-main README.md >>$(LOG_FILE)
+ mv frontend-main/* . >>$LOG_FILE
+ mv static/* . >>$LOG_FILE
+ rm -rf frontend-main README.md >>$LOG_FILE
  statusCheck $?
 
  echo "copy nginx config"
- mv localhost.conf /etc/nginx/default.d/roboshop.conf >>$(LOG_FILE)
+ mv localhost.conf /etc/nginx/default.d/roboshop.conf >>$LOG_FILE
  statusCheck $?
 
  echo "restart nginx"
- systemctl restart nginx >>$(LOG_FILE)
+ systemctl restart nginx >>$LOG_FILE
  statusCheck $?
