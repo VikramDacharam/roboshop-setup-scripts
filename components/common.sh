85ea13462686831e7ed00f7d051cb3e28849ca4f
Vikram691 <dasaramvikram@gmail.com>
2022-05-09 11:23:14 +0530

    addeded

--- components/common.sh
+++ components/common.sh
@@ -1,6 +1,8 @@
+#!/usr/bin/env bash
+
 checkRootUser() {
 
-USER_ID=$(id -u)
+  USER_ID=$(id -u)
 
 if [ "$User_ID" -ne "0" ]; then
   echo -e "\e[32myou are suppose to be running script as sudo or root checkRootUser\e[0m"
