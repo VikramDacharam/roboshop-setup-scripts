d35011099765cea1a754a512d1f46569bfdc3fc8
Vikram691 <dasaramvikram@gmail.com>
2022-05-09 13:22:40 +0530

    12

--- components/common.sh
+++ components/common.sh
@@ -9,7 +9,16 @@ checkRootUser(){
   fi
 }
 
-checkStatus(){
+
+statusCheck(){
+if [ $1 -eq 0 ]; then
+  echo -e "\e[32mSuccess\e[0m"
+else
+  echo -e "\e[32mFailure\e[0m"
+
+  exit 1
+  fi
+
+  }
 
 
-}
--- components/frontend.sh
+++ components/frontend.sh
@@ -6,28 +6,36 @@ checkRootUser
 
  echo "install nginx"
  yum install nginx -y >/tmp/roboshop.log
+ statusCheck $?
 
  echo "enable nginx"
  systemctl enable nginx >/tmp/roboshop.log
+ statusCheck $?
 
  echo "Downloading frontend code"
  curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >/tmp/roboshop.log
+ statusCheck $?
 
  cd /usr/share/nginx/html
 
  echo "remove old files"
  rm -rf * >/tmp/roboshop.log
+ statusCheck $?
 
  echo "extracting zip content"
  unzip /tmp/frontend.zip >/tmp/roboshop.log
+ statusCheck $?
 
  echo "move files"
  mv frontend-main/* . >/tmp/roboshop.log
  mv static/* . >/tmp/roboshop.log
  rm -rf frontend-main README.md >/tmp/roboshop.log
+ statusCheck $?
 
  echo "copy nginx config"
  mv localhost.conf /etc/nginx/default.d/roboshop.conf >/tmp/roboshop.log
+ statusCheck $?
 
  echo "restart nginx"
  systemctl restart nginx >/tmp/roboshop.log
+ statusCheck $?
