0a2ed6d59df8c4cc404b4529027e24da38cef18b
Vikram691 <dasaramvikram@gmail.com>
2022-05-09 16:26:21 +0530

    12

--- components/frontend.sh
+++ components/frontend.sh
@@ -4,38 +4,38 @@ source components/common.sh
 
 checkRootUser
 
- echo "install nginx"
+ ECHO "install nginx"
  yum install nginx -y >>$LOG_FILE
  statusCheck $?
 
- echo "enable nginx"
+ ECHO "enable nginx"
  systemctl enable nginx >>$LOG_FILE
  statusCheck $?
 
- echo "Downloading frontend code"
+ ECHO "Downloading frontend code"
  curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >>$LOG_FILE
  statusCheck $?
 
  cd /usr/share/nginx/html
 
- echo "remove old files"
+ ECHO "remove old files"
  rm -rf * >>$LOG_FILE
  statusCheck $?
 
- echo "extracting zip content"
+ ECHO "extracting zip content"
  unzip /tmp/frontend.zip >>$LOG_FILE
  statusCheck $?
 
- echo "move files"
+ ECHO "move files"
  mv frontend-main/* . >>$LOG_FILE
  mv static/* . >>$LOG_FILE
  rm -rf frontend-main README.md >>$LOG_FILE
  statusCheck $?
 
- echo "copy nginx config"
+ ECHO "copy nginx config"
  mv localhost.conf /etc/nginx/default.d/roboshop.conf >>$LOG_FILE
  statusCheck $?
 
- echo "restart nginx"
+ ECHO "restart nginx"
  systemctl restart nginx >>$LOG_FILE
  statusCheck $?
