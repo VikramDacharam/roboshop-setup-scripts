4ddf1331f172b49dac58950bd763b57dad979de0
Vikram691 <dasaramvikram@gmail.com>
2022-05-14 14:11:28 +0530

    12

--- create-ec2.sh
+++ create-ec2.sh
@@ -20,12 +20,12 @@ echo ec2 instance created
 
 sleep 10
 
-INSTANCE_ID=$( aws ec2 describe-spot-instance-requests --filters Name=tag:Name,Values=cart Name=state,Values=active --output table | grep InstanceId | awk '{print $4}')
+INSTANCE_ID=$(aws ec2 describe-spot-instance-requests --filters Name=tag:Name,Values=cart Name=state,Values=active --output table | grep InstanceId | awk '{print $4}')
 
 IPADDRESS=$(aws ec2 describe-instances --instance-ids ${INSTANCE_ID} --output table | grep PrivateIpAddress | head -n 1 | awk '{print $4}')
 
 sed -e "/s/COMPONENT/${NAME}/" -e "/s/IPADDRESS/${IPADDRESS}/" record.json >/tmp/record.json
-aws route53 change-resource-record-sets --hosted-zone-id Z0412959164KSIYBRR5N9 --change-batch file:///tmp/record.json &>/dev/null
+aws route53 change-resource-record-sets --hosted-zone-id Z0412959164KSIYBRR5N9 --change-batch file:///tmp/record.json
 echo DNS record created
 
 
