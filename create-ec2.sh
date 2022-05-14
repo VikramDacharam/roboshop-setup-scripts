f9af40b0752c525a9263d09250e5be6bd2645c6a
Vikram691 <dasaramvikram@gmail.com>
2022-05-14 14:51:57 +0530

    12

--- create-ec2.sh
+++ create-ec2.sh
@@ -18,13 +18,13 @@ AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Pra
 aws ec2 run-instances --image-id "${AMI_ID}" --instance-type t3.micro --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${NAME}}]" "ResourceType=instance,Tags=[{Key=Name,Value=${NAME}}]" &>/dev/null
 echo ec2 instance created
 
-sleep 10
+sleep 60
 
 INSTANCE_ID=$(aws ec2 describe-spot-instance-requests --filters Name=tag:Name,Values=cart Name=state,Values=active --output table | grep InstanceId | awk '{print $4}')
 
 IPADDRESS=$(aws ec2 describe-instances --instance-ids ${INSTANCE_ID} --output table | grep PrivateIpAddress | head -n 1 | awk '{print $4}')
 
-sed -e "/s/COMPONENT/${NAME}/" -e "/s/IPADDRESS/${IPADDRESS}/" record.json >/tmp/record.json
+ sed -e "/s/COMPONENT/${NAME}/" -e "/s/IPADDRESS/${IPADDRESS}/" record.json >/tmp/record.json
 aws route53 change-resource-record-sets --hosted-zone-id Z02287642UQYKFGW743KN --change-batch file:///tmp/record.json
 echo DNS record created
 
