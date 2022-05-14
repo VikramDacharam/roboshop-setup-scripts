2d3d93d9d90fa35d00eb68a3e1b56ba51f197110
Vikram691 <dasaramvikram@gmail.com>
2022-05-14 12:24:27 +0530

    12

--- create-ec2.sh
+++ create-ec2.sh
@@ -18,3 +18,7 @@ AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Pra
 aws ec2 run-instances --image-id "${AMI_ID}" --instance-type t3.micro --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${NAME}}]" "ResourceType=instance,Tags=[{Key=Name,Value=${NAME}}]" &>/dev/null
 
 
+
+
+
+
