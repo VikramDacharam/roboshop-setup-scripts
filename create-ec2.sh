95802736555d15075ff16e8afe78b47f3e4dd710
Vikram691 <dasaramvikram@gmail.com>
2022-05-15 19:17:40 +0530

    12

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
 if [ -z "$1" ]; then
-
   echo "instance name as argument is needed"
+  exit 1
 fi
 
-NAME=$1
-
+if [ "$1" == "list" ]; then
+ aws ec2 describe-instances --query "Reservations[*].Instances[*].{PrivateIp:PrivateIpAddress,PublicIp:PublicIpAddress,Name:Tags[?Key=='Name']|[0.Value, Status:state.name}" --output table
+ exit 0
+fi
 aws ec2 describe-spot-instance-requests --filters Name=tag:Name,Values=${NAME} Name=state,Values=active --output table | grep InstanceId &>/dev/null
 if [ $? -eq 0 ]; then
 echo "instance alrdy exists"
@@ -18,17 +20,15 @@ AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Pra
 aws ec2 run-instances --image-id "${AMI_ID}" --instance-type t3.micro --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${NAME}}]" "ResourceType=instance,Tags=[{Key=Name,Value=${NAME}}]" &>/dev/null
 echo ec2 instance created
 
-sleep 10
+sleep 30
 
 INSTANCE_ID=$(aws ec2 describe-spot-instance-requests --filters Name=tag:Name,Values=cart Name=state,Values=active --output table | grep InstanceId | awk '{print $4}')
 
 IPADDRESS=$(aws ec2 describe-instances --instance-ids ${INSTANCE_ID} --output table | grep PrivateIpAddress | head -n 1 | awk '{print $4}')
 
  sed -e "s/COMPONENT/${NAME}/" -e "s/IPADDRESS/${IPADDRESS}/" record.json >/tmp/record.json
-aws route53 change-resource-record-sets --hosted-zone-id Z02287642UQYKFGW743KN --change-batch file:///tmp/record.json
+aws route53 change-resource-record-sets --hosted-zone-id Z02287642UQYKFGW743KN --change-batch file:///tmp/record.json &>/dev/null
 echo DNS record created
-
-
 
 
 
