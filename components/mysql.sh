bf2d5ac390376c786a50512ea439fff85c1fb876
Vikram691 <dasaramvikram@gmail.com>
2022-05-24 12:04:02 +0530

    12

---
+++ components/mysql.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+source components/common.sh
+checkRootUser
+
+# systemctl enable mysqld
+# systemctl start mysqld
+# grep temp /var/log/mysqld.log
+# mysql_secure_installation
+# mysql -uroot -pRoboShop@1
+> uninstall plugin validate_password;
+# curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
+# cd /tmp
+# unzip mysql.zip
+# cd mysql-main
+# mysql -u root -pRoboShop@1 <shipping.sql
+
+ECHO "setup mysql repo"
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>${LOG_FILE}
+statusCheck $?
+
+ECHO "install mysql"
+yum install mysql-community-server -y &>>${LOG_FILE}
+statusCheck $?
+
+ECHO "start mysql"
+systemctl enable mysqld &>>${LOG_FILE} && systemctl start mysqld &>>${LOG_FILE}
+statusCheck $?
+
+
+
