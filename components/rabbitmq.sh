ac936165779a797f75be9e079d72fd273c3186dc
Vikram691 <dasaramvikram@gmail.com>
2022-05-27 17:30:58 +0530

    12

--- components/rabbitmq.sh
+++ components/rabbitmq.sh
@@ -25,8 +25,15 @@ ECHO "start rabbitmq server"
 systemctl enable rabbitmq-server &>>${LOG_FILE} && systemctl start rabbitmq-server &>>${LOG_FILE}
 statusCheck $?
 
+rabbitmqctl list_users | grep roboshop &>>${LOG_FILE}
+if [ $? -ne 0 ]; then
 ECHO "create an application user"
 rabbitmqctl add_user roboshop roboshop123 &>>${LOG_FILE}
+statusCheck $?
+fi
+
+ECHO "setup application user permission"
+rabbitmqctl set_user_tags roboshop administrator &>>${LOG_FILE} && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>{LOG_FILE}
 statusCheck $?
 
 
