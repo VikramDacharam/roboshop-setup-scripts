2b346287880e9021c406b9f71977bbbe19068270
Vikram691 <dasaramvikram@gmail.com>
2022-05-27 16:19:34 +0530

    12

---
+++ components/rabbitmq.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+source components/common.sh
+checkRootUser
+
+# yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y
+# curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
+
+# yum install rabbitmq-server -y
+# systemctl enable rabbitmq-server
+# systemctl start rabbitmq-server
+# rabbitmqctl add_user roboshop roboshop123
+# rabbitmqctl set_user_tags roboshop administrator
+# rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+
+ECHO "setup yum repos"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>${LOG_FILE}
+statusCheck $?
+
+ECHO "install rabbitmq& Erlang"
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm rabbitmq-server -y &>>${LOG_FILE}
+statusCheck $?
+
+ECHO "start rabbitmq server"
+systemctl enable rabbitmq-server &>>${LOG_FILE} && systemctl start rabbitmq-server &>>${LOG_FILE}
+statusCheck $?
+
+ECHO "create an application user"
+rabbitmqctl add_user roboshop roboshop123 &>>${LOG_FILE}
+statusCheck $?
+
+
+
+
+
+
