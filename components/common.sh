e4fb1256a72a2981a65f2652f735a53918738463
Vikram691 <dasaramvikram@gmail.com>
2022-05-27 18:31:46 +0530

    12

--- components/common.sh
+++ components/common.sh
@@ -52,8 +52,9 @@ statusCheck $?
 }
 
 Systemd_setup(){
+chown roboshop:roboshop /home/roboshop/${COMPONENT} -R
 ECHO "update systemd configure files"
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e "s/CARTENDPOINT/cart.roboshop.internal/" -e "s/DBHOST/mysql.roboshop.internal/" -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e "s/CARTENDPOINT/cart.roboshop.internal/" -e "s/DBHOST/mysql.roboshop.internal/" -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e "s/CARTHOST/cart.roboshop.internal/" -e 's/USERHOST/user.roboshop.internal/' -e 's/AMQPHOST/rabbitmq.roboshop.internal' /home/roboshop/${COMPONENT}/systemd.service
 statusCheck $?
 
 ECHO "setup systemd service"
@@ -94,5 +95,20 @@ Java(){
   statusCheck $?
 
 Systemd_setup
+
+}
+
+Python(){
+
+  ECHO " install python"
+   yum install python36 gcc python3-devel -y &>>${LOG_FILE}
+
+   Application_Setup
+
+    ECHO "compile maven package"
+    cd /home/roboshop/${COMPONENT} && pip3 install -r requirements.txt &>>${LOG_FILE}
+     statusCheck $?
+
+   Systemd_setup
 
 }
